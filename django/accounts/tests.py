0f364ea7e30e9d649004793616248afa3a0d1d48
lauryn-anderson <laurynelysse@gmail.com>
2022-05-23 10:49:43 -0600

    Create Django project with Docker
    Following William S. Vincent’s patterns laid out in “Django for Professionals”
    
    Includes CustomUser model extending AbstractUser

---
+++ django/accounts/tests.py
@@ -0,0 +1,32 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class CustomUserTests(TestCase):
+
+    def test_create_user(self):
+
+        user = get_user_model().objects.create_user(
+            username='user',
+            email='user@email.com',
+            password='testpass123'
+        )
+
+        self.assertEqual(user.username, 'user')
+        self.assertEqual(user.email, 'user@email.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+
+        admin_user = get_user_model().objects.create_superuser(
+            username='superadmin',
+            email='superadmin@email.com',
+            password='testpass123'
+        )
+        self.assertEqual(admin_user.username, 'superadmin')
+        self.assertEqual(admin_user.email, 'superadmin@email.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
